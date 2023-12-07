34935b2d9e07a8fa3b237927dda10055befeaa96
Greg Schuman <gcschuman@hotmail.com>
2023-12-07 13:29:39 -0800

    Began CSS refactoring.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"; 
-import "./App.css"; // Import your CSS file here
+import "./App.css"; 
 // import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col'; 
